3c8ccfc2965fe6e8bf2086f016cf51b2595cdd79
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-07-11 17:41:09 -0300

    se agregaron las validaciones de crear y editar receta

--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -7,15 +7,19 @@ import {
   obtenerListaRecetas,
   obtenerReceta,
 } from '../controllers/recetas.controllers';
+import validarReceta from '../helpers/validarReceta';
 
 const router = new Router();
 
 router.route('/').get(controladorTest);
-router.route('/recetas').post(crearReceta).get(obtenerListaRecetas);
+router
+  .route('/recetas')
+  .post(validarReceta, crearReceta)
+  .get(obtenerListaRecetas);
 router
   .route('/recetas/:id')
   .get(obtenerReceta)
   .delete(borrarReceta)
-  .put(editarReceta);
+  .put(validarReceta, editarReceta);
 
 export default router;
